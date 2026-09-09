d90db077a5d56f614bc4fdf4db1ad77e14c15a8c
haishhun <dmitryhaishun@gmail.com>
2026-09-09 10:29:35 -0400

    Configured database settings and updated requirements.txt

--- app/database.py
+++ app/database.py
@@ -11,6 +11,9 @@ DATABASE_URL = os.getenv("POSTGRES_URL_NON_POOLING")
 if not DATABASE_URL:
     raise ValueError("POSTGRES_URL_NON_POOLING is not set")
 
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(
